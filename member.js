447e2ccb5e9c61971d9c2603ac6de584bb202663
Ashish Soni <ashish.soni2091@gmail.com>
2024-03-28 22:17:35 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,31 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var memberValue = member.options[member.selectedIndex].value;
+  var memberText = member.options[member.selectedIndex].text;
+  var memberSkills = document.getElementById("memberSkills");
+  var memberSkillsValue = memberSkills.options[memberSkills.selectedIndex].value;
+  var memberSkillsText = memberSkills.options[memberSkills.selectedIndex].text;
+  var memberSkillsValueArray = memberSkillsValue.split(",");
+  var memberSkillsTextArray = memberSkillsText.split(",");
+  var memberSkillsValueArrayLength = memberSkillsValueArray.length;
+  var memberSkillsTextArrayLength = memberSkillsTextArray.length;
+  var memberSkillsValueArrayLengthMinusOne = memberSkillsValueArrayLength - 1;
+  var memberSkillsTextArrayLengthMinusOne = memberSkillsTextArrayLength - 1;
+  var memberSkillsValueArrayLengthMinusTwo = memberSkillsValueArrayLength - 2;
+  var memberSkillsTextArrayLengthMinusTwo = memberSkillsTextArrayLength - 2;
+  var memberSkillsValueArrayLengthMinusThree = memberSkillsValueArrayLength - 3;
+  var memberSkillsTextArrayLengthMinusThree = memberSkillsTextArrayLength - 3;
+  var memberSkillsValueArrayLengthMinusFour = memberSkillsValueArrayLength - 4;
+  var memberSkillsTextArrayLengthMinusFour = memberSkillsTextArrayLength - 4;
+  var memberSkillsValueArrayLengthMinusFive = memberSkillsValueArrayLength - 5;
+  var memberSkillsTextArrayLengthMinusFive = memberSkillsTextArrayLength - 5;
+  var memberSkillsValueArrayLengthMinusSix = memberSkillsValueArrayLength - 6;
+  var memberSkillsTextArrayLengthMinusSix = memberSkillsTextArrayLength - 6;
+  var memberSkillsValueArrayLengthMinusSeven = memberSkillsValueArrayLength - 7;
+  var memberSkillsTextArrayLengthMinusSeven = memberSkillsTextArrayLength - 7;
+  var memberSkillsValueArrayLengthMinusEight = memberSkillsValueArrayLength - 8;
+  var memberSkillsTextArrayLengthMinusEight = memberSkillsTextArrayLength - 8;
+  var memberSkillsValueArrayLengthMinusNine = memberSkillsValueArrayLength - 9;
+  var memberSkillsTextArrayLengthMinusNine = memberSkillsTextArrayLength - 9;
+  var memberSkillsValueArrayLengthMinusTen = memberSkillsValueArrayLength - 10;
+  var memberSkillsTextArrayLength
